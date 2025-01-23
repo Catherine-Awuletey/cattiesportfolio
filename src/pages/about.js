bd50aa172ecc19333f89bd28dfd362146efc5b8d
Cattie <87996502+Catherine-Awuletey@users.noreply.github.com>
2025-01-23 15:14:04 +0000

    Fix Dependencies

--- src/pages/about.js
+++ src/pages/about.js
@@ -21,7 +21,7 @@ const AnimatedNumbers = ({ value }) => {
     if (isInView) {
       motionValue.set(value);
     }
-  }, [isInView, value]);
+  }, [isInView, motionValue, value]); // Added missing dependencies
 
   useEffect(() => {
     springValue.on("change", (latest) => {
@@ -29,7 +29,7 @@ const AnimatedNumbers = ({ value }) => {
         ref.current.textContent = latest.toFixed(0);
       }
     });
-  }, [springValue]);
+  }, [springValue, value]); // Added missing dependency
 
   return <span ref={ref}></span>;
 };
@@ -58,7 +58,7 @@ const about = () => {
               <p className="font-medium">
                 Hi, I&apos;m CodeBucks, a web developer and UI/UX designer with a
                 passion for creating beautiful, functional, and user-centered
-                digital experiences. With 4 years of experience in the field. I
+                digital experiences. With 4 years of experience in the field, I
                 am always looking for new and innovative ways to bring my
                 clients&apos; visions to life.
               </p>
@@ -66,7 +66,7 @@ const about = () => {
               <p className="my-4 font-medium">
                 I believe that design is about more than just making things look
                 pretty – it&apos;s about solving problems and creating intuitive,
-                enjoyable experiences for users.{" "}
+                enjoyable experiences for users.
               </p>
 
               <p className="font-medium">
@@ -103,7 +103,7 @@ bg-light p-8 dark:bg-dark dark:border-light xl:col-span-4 md:order-1 md:col-span
               </div>
               <div className="flex flex-col items-end justify-center xl:items-center">
                 <span className="inline-block text-7xl font-bold md:text-6xl sm:text-5xl xs:text-4xl">
-                  <AnimatedNumbers value={15} /> +
+                  <AnimatedNumbers value={15} />+
                 </span>
                 <h2 className="text-xl font-medium capitalize text-dark/75 dark:text-light/75  xl:text-center md:text-lg sm:text-base xs:text-sm">
                   projects completed
